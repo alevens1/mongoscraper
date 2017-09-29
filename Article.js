baa077abb260050dde4bd122a9e4d5cd0c58c55d
alevens1 <alevens1@icloud.com>
2017-09-29 13:12:25 -0400

    Creat Article.js

---
+++ Article.js
@@ -0,0 +1,39 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var ArticleSchema = new Schema({
+
+  title: {
+    type: String,
+    required: true
+  },
+
+  link: {
+    type: String,
+    required: true
+  },
+  
+  summary: {
+    type: String,
+    required: true
+  },
+
+
+  updated: {
+    type: String,
+    default: moment().format('MMMM Do YYYY, h:mm A')
+  },
+
+  comments: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Comment'
+  }]
+
+});
+
+
+var Article = mongoose.model('Article', ArticleSchema);
+
+
+module.exports = Article;
